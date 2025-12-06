70fc73e0e991dbaf7500ccacbc2ce00c524be09e
Jaume Blanco González <jaumeblancog@gmail.com>
2025-12-06 18:17:39 +0100

    Testing issues with Netlify Forms - part 4

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,31 +11,3 @@
  */
 
 import * as bootstrap from 'bootstrap';
-
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector(".formulario");
-  const modalExito = new bootstrap.Modal(document.getElementById("modal-exito-formulario"));
-  const modalError = new bootstrap.Modal(document.getElementById("modal-error-formulario"));
-
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-
-    if (!form.reportValidity()) return;
-
-    const formData = new FormData(form);
-
-    try {
-      await fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(formData).toString()
-      });
-
-      modalExito.show();
-      form.reset();
-
-    } catch (error) {
-      modalError.show();
-    }
-  })
-})
